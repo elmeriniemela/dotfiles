bdae1dd041c90989c444860a08da1caeb43f5e45
Elmeri Niemelä <niemela.elmeri@gmail.com>
2021-06-12 09:56:39 +0300

    Add .backup_home.sh

---
+++ .backup_home.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rsync -avz --delete .config/syncthing/ elmeri.asuscomm.com:.config/syncthing
+rsync -avz --delete .mozilla/ elmeri.asuscomm.com:.mozilla
+rsync -avz --delete .thunderbird/ elmeri.asuscomm.com:.thunderbird
+rsync -avz --delete School/ elmeri.asuscomm.com:School
+rsync -avz --delete Projects/ elmeri.asuscomm.com:Projects
+rsync -avz --delete Videos/ elmeri.asuscomm.com:Videos
+rsync -avz --delete Documents/ elmeri.asuscomm.com:Documents
+
+rsync -avz --delete *.ovpn elmeri.asuscomm.com:
+rsync -avz --delete .bash_eternal_history elmeri.asuscomm.com:
+rsync -avz --delete .psql_history elmeri.asuscomm.com:
+rsync -avz --delete .python_history  elmeri.asuscomm.com:
